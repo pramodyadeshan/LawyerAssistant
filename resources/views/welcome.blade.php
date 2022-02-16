a629b0e87563065a038356b2fa7e348ad1669e9d
acer <pramodyadeshan1996@gmail.com>
2022-02-16 16:57:08 +0530

    aaaa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,7 @@
                     </svg>
                 </div>
 
-                <h1> Success Project</h1>
+                <h1> Success Project asdasdas</h1>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
